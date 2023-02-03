0456dccc98c81998bf49efc2b70147d6ce628c3d
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-02-03 23:52:38 +0300

    CI

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:18 AS build
+ARG BUILD_MODE
+WORKDIR /app
+ADD ./package.json yarn.lock /app/
+RUN corepack enable && yarn install --frozen-lockfile --link-duplicates
+ADD . /app
+RUN yarn build --mode ${BUILD_MODE}
+
+FROM nginx:1.21
+ADD ./default.conf /etc/nginx/conf.d/default.conf
+ADD ./docker_entrypoint.sh /docker_entrypoint.sh
+COPY --from=build /app/dist /usr/share/nginx/html
+RUN chmod +x /docker_entrypoint.sh && /docker_entrypoint.sh
--- src/main.js
+++ src/main.js
@@ -8,16 +8,7 @@ const vueLifecycles = singleSpaVue({
   createApp,
   appOptions: {
     render() {
-      return h(App, {
-        // single-spa props are available on the "this" object. Forward them to your component as needed.
-        // https://single-spa.js.org/docs/building-applications#lifecycle-props
-        // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
-        /*
-        name: this.name,
-        mountParcel: this.mountParcel,
-        singleSpa: this.singleSpa,
-        */
-      });
+      return h(App, {});
     },
   },
   handleInstance(app) {
