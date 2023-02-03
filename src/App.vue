ba23814ca6e7b696c83da5a2e3eba9bdabf0f8fe
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-02-03 23:22:58 +0300

    Devtools init project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <img alt="Vue logo" :src="url">
+  <HelloWorld msg="Ура! Вы создали приложение для Твой ФФ!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    url: `${process.env.VUE_APP_CDN}/app/logo/logo_ff.svg`
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+  
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+      runtimeCompiler: true,
+      configureWebpack: {
+        output: {
+            libraryTarget: 'system',
+        },
+        devServer: {
+            hot: false,
+            liveReload: false
+        },
+        externals: ["single-spa", "vue", "vue-router"]
+    },
+};
