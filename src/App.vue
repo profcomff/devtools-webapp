e2011d3a418330b138531efdd17d12d880f22278
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-02-04 17:32:14 +0300

    Push settings

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,3 @@
-
 <template>
-  <img alt="Vue logo" :src="url">
-  <HelloWorld msg="Ура! Вы создали приложение для Твой ФФ!"/>
+    <RouterView />
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    url: `${process.env.VUE_APP_CDN}/app/logo/logo_ff.svg`
-  })
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <h1>Настройки уведомлений</h1>
+        <div class="home">
+            <div class="form-check form-switch">
+                <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                    v-model="push_enable"
+                />
+                <label
+                    class="form-check-label"
+                    for="flexSwitchCheckDefault"
+                    >Тестовые пуш уведомления</label
+                >
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    data: () => ({
+        push_enable: localStorage.getItem('devtools_push_enable') || false,
+    }),
+    watch: {
+        push_enable(newstate) {
+            localStorage.setItem('devtools_push_enable', newstate);
+            dispatchEvent(
+                new Event(
+                    newstate ? 'devtools-push-enable' : 'devtools-push-disable',
+                ),
+            );
+        },
+    },
+};
 </script>
